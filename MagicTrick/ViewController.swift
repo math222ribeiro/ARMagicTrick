e95733d65a065e10ef240b9cdf323bd06e72ccd5
Matheus Ribeiro D'Azevedo Lopes <math222ribeiro@hotmail.com>
2018-03-21 01:35:26 -0300

    Updating anchor planes

--- MagicTrick/ViewController.swift
+++ MagicTrick/ViewController.swift
@@ -103,11 +103,25 @@ extension ViewController: ARSCNViewDelegate {
     plane.materials = [planeMaterial]
     
     let alphaPlane = SCNNode(geometry: plane)
-    alphaPlane.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
-    
+    alphaPlane.position = SCNVector3Make(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
     alphaPlane.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+    let shape = SCNPhysicsShape(geometry: plane, options: nil)
+    alphaPlane.physicsBody = SCNPhysicsBody(type: .static, shape: shape)
     
-//    node.addChildNode(alphaPlane)
+    node.addChildNode(alphaPlane)
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    guard let planeAnchor = anchor as? ARPlaneAnchor,
+          let alphaPlane = node.childNodes.first,
+          let plane = alphaPlane.geometry as? SCNPlane
+    else { return }
+    
+    plane.width = CGFloat(planeAnchor.extent.x)
+    plane.height = CGFloat(planeAnchor.extent.z)
+    alphaPlane.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+    let shape = SCNPhysicsShape(geometry: plane, options: nil)
+    alphaPlane.physicsBody = SCNPhysicsBody(type: .static, shape: shape)
   }
 
 }
